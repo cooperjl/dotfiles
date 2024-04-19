08528e248d60e52637f8ecc29dd4683361bfeaa0
Cooper Love <cooperjlove@proton.me>
2024-04-19 16:27:02 +0100

    Removed old and x11 related dotfiles

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require('plugins')
-require('options')
-require('lsp')
-require('colorscheme')
-require('mason').setup()
-require('mason-lspconfig').setup {
-    ensure_installed = { 'pylsp', 'rust_analyzer', "clangd" },
-}
